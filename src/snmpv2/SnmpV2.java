34dd307dd0e8278fc20b5f5047fa0aaece6a8973
Revoly <68769911+afanasevrev@users.noreply.github.com>
2022-01-09 18:38:13 +0900

    Update SnmpV2.java

--- src/snmpv2/SnmpV2.java
+++ src/snmpv2/SnmpV2.java
@@ -19,17 +19,16 @@ public class SnmpV2 {
     static String ULPR1 = "[1.3.6.1.4.1.12.3.1.0 = ULPR|3|NUMBER_DETECTED->ARM:WIN-6JF3PBLIACQ,UserId:,Module:urmlpr.run]";
     static String ULPR2 = "[1.3.6.1.4.1.12.3.1.0 = ULPR|4|NUMBER_DETECTED->ARM:WIN-6JF3PBLIACQ,UserId:,Module:urmlpr.run]";
     static String UnipingIpaddress_1 = "192.168.0.102";
-    static String name_1 = "Центральный въезд";
-  //  static String ipaddr_1 = "udp:192.168.1.51/162";
-    static String ipaddr_1 = "udp:10.64.32.198/162";
+    static String name_1 = "Шлагбаум №1";
+    static String ipaddr_1 = "udp:10.10.10.11/162";
     
     static String LPRDB2 = "[1.3.6.1.4.1.12.3.1.0 = LPRDB|1|SEARCH_RESULT->ARM:WIN-6JF3PBLIACQ,UserId:,Module:lprdb.run]";
     static String ULPR3 = "[1.3.6.1.4.1.12.3.1.0 = ULPR|1|NUMBER_DETECTED->ARM:WIN-6JF3PBLIACQ,UserId:,Module:urmlpr.run]";
     static String ULPR4 = "[1.3.6.1.4.1.12.3.1.0 = ULPR|2|NUMBER_DETECTED->ARM:WIN-6JF3PBLIACQ,UserId:,Module:urmlpr.run]";
     static String UnipingIpaddress_2 = "192.168.0.101";
-    static String name_2 = "Задний двор";
+    static String name_2 = "Шлагбаум №2";
     static String ipaddr_2 = "udp:127.0.0.1/162";
-   // static String ipaddr_2 = "udp:10.64.32.198/161";
+  
     public static void main(String[] args) throws Exception {
         // TODO code application logic here
         SnmpWrapper firstThread = new SnmpWrapper(LPRDB1,ULPR1,ULPR2,UnipingIpaddress_1,name_1,ipaddr_1);
